e223b321cf9fa802ef5df96105d8bf5083d39904
casperfrolin-bit <casper.frolin@gmail.com>
2026-05-11 20:20:36 +0200

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,419 @@
+const menuData = {
+
+1: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+2: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+3: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+4: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+5: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+6: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+7: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+8: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+9: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+10: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+11: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+12: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+13: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+14: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+15: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+16: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+17: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+18: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+19: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+20: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+21: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+22: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+23: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+24: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+25: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+26: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+27: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+28: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+29: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+30: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+31: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+32: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+33: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+34: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+35: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+36: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+37: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+38: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+39: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+40: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+41: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+42: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+43: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+44: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+45: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+46: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+47: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+48: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+49: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+50: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+51: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}},
+
+52: { dagar: {
+måndag: { mat: ["", ""] },
+tisdag: { mat: ["", ""] },
+onsdag: { mat: ["", ""] },
+torsdag: { mat: ["", ""] },
+fredag: { mat: ["", ""] }
+}}
+
+};
